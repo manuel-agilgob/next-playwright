36ce5dbc065efbdc8836ee516c56a8f4d0120e93
Manuel valdez <manuel@agilgob.com>
2026-03-27 19:31:41 -0600

    builders

--- src/contracts/IParty.interface.ts
+++ src/contracts/IParty.interface.ts
@@ -1,38 +1,67 @@
-// Personal Information
+// Lists of options for select fields
 
+export const partyTypes = ['Actor' , 'Demandado' , 'Tercero Interesado' , 'Ministerio Público' , 'Victima u ofendido' , 'Imputado' , 'Acusado' ,
+      'Sentenciado' , 'De Cujus' , "Abogado patrono del actor"] as const;
 
+export const classificationTypes = ['Pública', 'Privada'] as const;
 
+export const partyRegimeTypes = ['Persona Física', 'Persona Moral'] as const;
+
+export const sexTypes = ['Masculino', 'Femenino'] as const;
+
+export const genderTypes = ['Masculino', 'Femenino'] as const;
+
+export const gradeOfStudiesTypes = ['Sin estudios', 'Primaria', 'Secundaria', 'Preparatoria', 'Licenciatura', 'Maestría', 'Doctorado'] as const;
+
+export const nationalityTypes = ['Mexicana', 'Estadounidense', 'Otra'] as const;
+
+export const civilStatusTypes = ['Soltero(a)', 'Casado(a)', 'Divorciado(a)', 'Viudo(a)'] as const;
+
+export const randomBooleans = ['Sí', 'No'] as const;
+
+// Type definitions for select fields
+
+type TypeOfParty = typeof partyTypes[number];
+type Classification = typeof classificationTypes[number];
+type PartyRegime = typeof partyRegimeTypes[number];
+type Gender = typeof genderTypes[number];
+type GradeOfStudies = typeof gradeOfStudiesTypes[number];
+type Nationality = typeof nationalityTypes[number];
+type CivilStatus = typeof civilStatusTypes[number];
+type Sex = typeof sexTypes[number];
+type RandomBoolean = typeof randomBooleans[number];
+
+// Personal Information Section
 export interface IPartyPersonal {
-  partyType: 'Actor' | 'Demandado' | 'Tercero Interesado' | 'Ministerio Público' | 'Victima u ofendido' | 'Imputado' | 'Acusado' |
-      'Sentenciado' | 'De Cujus' | "Abogado patrono del actor";
+  partyType: TypeOfParty; 
   names: string;
   paternalSurname: string;
   maternalSurname: string;
   birthDate: string;
-  sex: string;
-  clasification: 'Pública' | 'Privada';
+  sex: Sex;
+  clasification: Classification;
   alias: string;
   age: number;
-  partyRegime: 'Persona Física' | 'Persona Moral';
-  gender: 'Masculino' | 'Femenino';
+  partyRegime: PartyRegime;
+  gender: Gender;
 }
 
-// Contact Information
+// Contact Information Section
 export interface IPartyContact {
   email: string;
   phoneNumber: string;
   address: string;
 }
 
-// Transparency and legal information
+// Transparency and legal information Section
 export interface IPartyLegal {
-  canReadAndWrite: string;
-  gradeOfStudies: 'Sin estudios' | 'Primaria' | 'Secundaria' | 'Preparatoria' | 'Licenciatura' | 'Maestría' | 'Doctorado';
-  nationality: 'Mexicana' | 'Estadounidense' | 'Otra';
-  speaksSpanish: string;
-  civilStatus: 'Soltero(a)' | 'Casado(a)' | 'Divorciado(a)' | 'Viudo(a)';
+  canReadAndWrite: RandomBoolean;
+  gradeOfStudies: GradeOfStudies;
+  nationality: Nationality;
+  speaksSpanish: RandomBoolean;
+  civilStatus: CivilStatus;
   occupation: string;
-  belongsToIndigenousGroup: string;
+  belongsToIndigenousGroup: RandomBoolean;
 }
 
 // Combined interface
---
+++ src/data-builders/partyBuilder.ts
@@ -0,0 +1,50 @@
+import { randomInt } from 'node:crypto';
+import { faker } from '@faker-js/faker/locale/es_MX';
+import { 
+    IParty,
+    partyTypes,
+    sexTypes, 
+    classificationTypes,
+    genderTypes,
+    gradeOfStudiesTypes,
+    nationalityTypes,
+    civilStatusTypes,
+    partyRegimeTypes,
+    randomBooleans } from '@contracts/IParty.interface';
+
+
+export function buildPartyExample(overrides?: Partial<IParty>): IParty {
+    const defaultParty: IParty = {
+        // Personal Information
+        partyType: partyTypes[randomInt(0, partyTypes.length)],
+        names: faker.person.firstName(),
+        paternalSurname: faker.person.lastName(),
+        maternalSurname: faker.person.lastName(),
+        birthDate: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).toDateString(),
+        sex: sexTypes[randomInt(0, sexTypes.length)],
+        clasification: classificationTypes[randomInt(0, classificationTypes.length)],
+        alias: faker.internet.username(),
+        age: faker.number.int({ min: 18, max: 65 }),
+        partyRegime: partyRegimeTypes[randomInt(0, partyRegimeTypes.length)],
+        gender: genderTypes[randomInt(0, genderTypes.length)],
+
+        // Contact Information
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.number({style : 'national'}).replace(/\D/g, ''),
+        address: faker.location.streetAddress() + ', ' + faker.location.city() + ', ' + faker.location.state() + ', CP ' + faker.location.zipCode('######'),
+
+        // Transparency and legal information
+        canReadAndWrite: randomBooleans[randomInt(0, randomBooleans.length)],
+        gradeOfStudies: gradeOfStudiesTypes[randomInt(0, gradeOfStudiesTypes.length)],
+        nationality: nationalityTypes[randomInt(0, nationalityTypes.length)],
+        speaksSpanish: randomBooleans[randomInt(0, randomBooleans.length)],
+        civilStatus: civilStatusTypes[randomInt(0, civilStatusTypes.length)],
+        occupation: faker.person.jobTitle(),
+        belongsToIndigenousGroup: randomBooleans[randomInt(0, randomBooleans.length)],
+    };
+
+    return {
+        ...defaultParty,
+        ...overrides
+    };
+}
