d7357a18a2a1a7d156763a4941b6b0520c023eca
Manuel valdez <manuel@agilgob.com>
2026-03-30 11:02:32 -0600

    Commit para fusionar cambios

--- src/tests/e2e/expedient/create-expedient-from-functionary.spec.ts
+++ src/tests/e2e/expedient/create-expedient-from-functionary.spec.ts
@@ -2,8 +2,8 @@ import { test } from '@playwright/test';
 
 import { NavigationBar } from '@ui/components/NavigationBar';
 import { JudicialExpedientsPage } from '@ui/pages/JudicialExpedientsPage';
-import { GeneralInformationAboutExpedientForm } from '../../../ui/forms/GeneralInformationAboutExpedientForm';
-import { buildExpedient } from '../../../data-builders/expedients/expedientNumberBuilder';  
+import { GeneralInformationAboutExpedientForm } from '@ui/forms/GeneralInformationAboutExpedientForm';
+import { buildExpedient } from '@data-builders/expedientNumberBuilder';  
 import { assertExpedientGeneralInformationIsCorrect, assertExpedientNumberIsValid } 
     from '@assertions/createExpedientForm.assert';
 import { IExpedient } from '@contracts/IExpedient.interface';
--- src/tests/e2e/parties/create-parties.spec.ts
+++ src/tests/e2e/parties/create-parties.spec.ts
@@ -3,7 +3,7 @@ import { test, expect} from '@playwright/test';
 import { NavigationBar } from '@ui/components/NavigationBar';
 import { JudicialExpedientsPage } from '@ui/pages/JudicialExpedientsPage';
 import { GeneralInformationAboutExpedientForm } from '@ui/forms/GeneralInformationAboutExpedientForm';
-import { buildExpedient } from '@data-builders/expedients/expedientNumberBuilder';  
+import { buildExpedient } from '@data-builders/expedientNumberBuilder';  
 import { assertExpedientGeneralInformationIsCorrect, assertSummaryCardInformationIsCorrect } from '@assertions/createExpedientForm.assert';
 import { CreateNewExpedientPage } from '@ui/pages/CreateNewExpedientPage';
 import { IParty } from '@contracts/IParty.interface';
